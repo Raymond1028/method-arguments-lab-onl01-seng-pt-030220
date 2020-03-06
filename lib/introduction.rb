44aae9887ddcaab2111a3de93097a1fea1ecf958
Raymond Sims <lancelot53.rs@gmail.com>
2020-03-06 02:32:54 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  puts "Hi, my name is#{name}." 
+  end
+introduction("Josh")
+
+def introduction_language(name,language)
+  puts "Hi, my name is #{name} and I am learnig  to program in #{language}."
+  end
+  introduction_language("Dan," "Ember.jslearn")
